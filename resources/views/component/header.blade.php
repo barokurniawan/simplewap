3d6942493651f138875df16afd0f168cf386e262
barokurniawan <kurniawan9411n@gmail.com>
2019-09-04 00:51:08 +0700

    test jenkins

--- resources/views/component/header.blade.php
+++ resources/views/component/header.blade.php
@@ -5,7 +5,7 @@
                 <img src="logo.png" width="160" height="45" title="SimpleWAP CMS" alt="logo" />
             </td>
             <td style="text-align: right;">
-                English <a href="/?lang=en"><img src="data/lang/flags/en.gif" alt="*" /></a>
+                Sundanese <a href="/?lang=en"><img src="data/lang/flags/en.gif" alt="*" /></a>
             </td>
         </tr>
     </table>
