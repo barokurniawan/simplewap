cb18f89cddf2b78371d5d7a72db3d41fbf97ab75
barokurniawan <kurniawan9411n@gmail.com>
2019-09-04 01:41:18 +0700

    test jenkins

--- resources/views/component/bottom-section.blade.php
+++ resources/views/component/bottom-section.blade.php
@@ -1,10 +1,10 @@
 @forelse ($bottom_list as $item)
 @if ($item->type == "text")
-    <div class="title"><b>{{ $item->name }}</b></div>
+<div class="title"><b>{{ $item->name }}</b></div>
 @else
-    <div class="menu">
-        &nbsp;&nbsp;&nbsp;<a href="{{ $item->url }}" title="News">{{ $item->name }}</a>
-    </div>
-@endif    
+<div class="menu">
+    &nbsp;&nbsp;&nbsp;<a href="{{ asset($item->url) }}" title="News">{{ $item->name }}</a>
+</div>
+@endif
 @empty
 @endforelse
--- resources/views/component/header.blade.php
+++ resources/views/component/header.blade.php
@@ -5,7 +5,7 @@
                 <img src="logo.png" width="160" height="45" title="SimpleWAP CMS" alt="logo" />
             </td>
             <td style="text-align: right;">
-                Sundanese <a href="/?lang=en"><img src="data/lang/flags/en.gif" alt="*" /></a>
+                EN <a href="/?lang=en"><img src="data/lang/flags/en.gif" alt="*" /></a>
             </td>
         </tr>
     </table>
